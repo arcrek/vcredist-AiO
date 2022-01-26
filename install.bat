364c31b108db0feb000786b96e5d6cd455bbc5a3
Nguyen Dang Dat <64296818+nguyendang-dat@users.noreply.github.com>
2022-01-27 01:15:06 +0700

    Update install.bat

--- install.bat
+++ install.bat
@@ -4,126 +4,88 @@ title "Microsoft Visual C++ Redistributable All-in-One  -  fb.com/nguyendangdat.
 mode con lines=30 cols=115
 CD /d %~dp0bin
 call "%~dp0bin\permissions.bat"
+call "%~dp0bin\language.bat"
 
-echo.
-echo Microsoft Visual C++ Redistributable All-in-One
-echo https://github.com/nguyendang-dat/vcredist-AiO
-echo.
-echo Installing packages...
-echo.
+:SRT
+
+cls & echo. & echo Microsoft Visual C++ Redistributable All-in-One & echo https://github.com/nguyendang-dat/vcredist-AiO & echo. & echo %_001_% & echo.
 
 set IS_X64=0 && if "%PROCESSOR_ARCHITECTURE%"=="AMD64" (set IS_X64=1) else (if "%PROCESSOR_ARCHITEW6432%"=="AMD64" (set IS_X64=1))
 
 if "%IS_X64%" == "1" goto X64
 
-echo -------------------------------------------------
-echo.
-echo Installing vcredist_2005_x86...
+echo %_001_% & echo. & echo %_003_%
 start /wait vcredist_x86_2005.exe /q
 echo.
 
-echo -------------------------------------------------
-echo.
-echo Installing vcredist_2008_x86...
+echo %_001_% & echo. & echo %_005_%
 start /wait vcredist_x86_2008.exe /qb
 echo.
 
-echo -------------------------------------------------
-echo.
-echo Installing vcredist_2010_x86...
+echo %_001_% & echo. & echo %_007_%
 start /wait vcredist_x86_2010.exe /passive /norestart
 echo.
 
-echo -------------------------------------------------
-echo.
-echo Installing vcredist_2012_x86...
+echo %_001_% & echo. & echo %_009_%
 start /wait vcredist_x86_2012.exe /passive /norestart
 echo.
 
-echo -------------------------------------------------
-echo.
-echo Installing vcredist_2013_x86...
+echo %_001_% & echo. & echo %_011_%
 start /wait vcredist_x86_2013.exe /passive /norestart
 echo.
 
-echo -------------------------------------------------
-echo.
-echo Installing vcredist_2015-2022_x86...
+echo %_001_% & echo. & echo %_013_%
 start /wait VC_redist.x86_2015-2022.exe /passive /norestart
 
-goto END
+goto :END
 
 :X64
 
-echo -------------------------------------------------
-echo.
-echo Installing vcredist_2005_x86...
+echo %_001_% & echo. & echo %_003_%
 start /wait vcredist_x86_2005.exe /q
 echo.
 
-echo -------------------------------------------------
-echo.
-echo Installing vcredist_2005_x64...
+echo %_001_% & echo. & echo %_004_%
 start /wait vcredist_x64_2005.exe /q
 echo.
 
-echo -------------------------------------------------
-echo.
-echo Installing vcredist_2008_x86...
+echo %_001_% & echo. & echo %_005_%
 start /wait vcredist_x86_2008.exe /qb
 echo.
 
-echo -------------------------------------------------
-echo.
-echo Installing vcredist_2008_x64...
+echo %_001_% & echo. & echo %_006_%
 start /wait vcredist_x64_2008.exe /qb
 echo.
 
-echo -------------------------------------------------
-echo.
-echo Installing vcredist_2010_x86...
+echo %_001_% & echo. & echo %_007_%
 start /wait vcredist_x86_2010.exe /passive /norestart
 echo.
 
-echo -------------------------------------------------
-echo.
-echo Installing vcredist_2010_x64...
+echo %_001_% & echo. & echo %_008_%
 start /wait vcredist_x64_2010.exe /passive /norestart
 echo.
 
-echo -------------------------------------------------
-echo.
-echo Installing vcredist_2012_x86...
+echo %_001_% & echo. & echo %_009_%
 start /wait vcredist_x86_2012.exe /passive /norestart
 echo.
 
-echo -------------------------------------------------
-echo.
-echo Installing vcredist_2012_x64...
+echo %_001_% & echo. & echo %_010_%
 start /wait vcredist_x64_2012.exe /passive /norestart
 echo.
 
-echo -------------------------------------------------
-echo.
-echo Installing vcredist_2013_x86...
+echo %_001_% & echo. & echo %_011_%
 start /wait vcredist_x86_2013.exe /passive /norestart
 echo.
 
-echo -------------------------------------------------
-echo.
-echo Installing vcredist_2013_x64...
+echo %_001_% & echo. & echo %_012_%
 start /wait vcredist_x64_2013.exe /passive /norestart
 echo.
 
-echo -------------------------------------------------
-echo.
-echo Installing vcredist_2015-2022_x86...
+echo %_001_% & echo. & echo %_013_%
 start /wait VC_redist.x86_2015-2022.exe /passive /norestart
 echo.
 
-echo -------------------------------------------------
-echo.
-echo Installing vcredist_2015-2022_x64...
+echo %_001_% & echo. & echo %_014_%
 start /wait VC_redist.x64_2015-2022.exe /passive /norestart
 
 goto END
@@ -131,14 +93,7 @@ goto END
 :END
 
 cls
-echo.
-echo -------------------------------------------------
-echo.
-echo Installation completed successfully.
-echo Thank you for using this script.
-echo Press any key to exit...
-echo.
-echo -------------------------------------------------
+echo. & echo %_001_% & echo. & echo %_015_% & echo %_016_% & echo %_017_% & echo. & echo %_001_%
 pause >nul
 
 exit
